c1c9f4ae4cf7aad638eea8a198623921d18a77ab
wanjirumephis <wanjirunduhi@gmail.com>
2023-02-17 11:26:29 -0800

    a C program that checks if a number is less than 6

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,30 +5,28 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
- */
+ * Description: print the value of n status:
+ *              greater than, is zero and is not less than 6.
+ *
+ * Return: Always O (Success)
+*/
+
 int main(void)
 {
-    int n;
-    int m;
+	int n, digit;
 
-    srand(time(0));
-    n = rand() - RAND_MAX / 2;
-    m = n % 10;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
 
-    if (m > 5)
-    {
-        printf("last digit of %d is %d and is greater than 5\n", n, m);
-    }
-    else if (m == 0)
-    {
-        printf("last digit of %d is %d and is 0\n", n, m);
-    }
-    else
-    {
-        printf("last digit of %d is %d and is less than 6 and not 0\n", n, m);
-    }
+	/*my code*/
+	digit = n % 10;
 
-    return (0);
+	if (digit > 5)
+		printf("Last digit of %i is %i and is greater than 5\n", n, digit);
+	else if (digit == 0)
+		printf("Last digit of %i is %i and is 0\n", n, digit);
+	else if (digit < 6 && digit != 0)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit);
+
+	return (0);
 }
-
