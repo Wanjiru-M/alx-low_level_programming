9d3b352b1a523520e45ebe8de21bf64241ba9333
wanjirumephis <wanjirunduhi@gmail.com>
2023-04-10 05:16:16 -0700

    Fixed indenations

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,27 +7,26 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int num = 0;
-int i;
+	unsigned int num = 0;
+	int i;
 
-if (b == NULL)
-return (0);
+	if (b == NULL)
+		return (0);
 
-for (i = 0; b[i] != '\0'; i++)
-{
-switch (b[i])
-{
-case '0':
-num <<= 1;
-break;
-case '1':
-num <<= 1;
-num |= 1;
-break;
-default:
-return (0);
-}
-}
-
-return (num);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		switch (b[i]
+        {
+		       case '0':
+			num <<= 1;
+			break;
+			case '1'
+				num <<= 1;
+			num |= 1;
+			break;
+			default:
+			return (0);
+		}
+	}
+	return (num);
 }
