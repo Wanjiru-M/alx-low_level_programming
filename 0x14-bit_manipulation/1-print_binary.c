268c7f212c55ec647a0645b0a9c3b88783e8ff40
wanjirumephis <wanjirunduhi@gmail.com>
2023-04-10 06:59:41 -0700

    checker fix

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -2,23 +2,16 @@
 #include <stdio.h>
 
 /**
- * print_binary - printts binary representation of a no
- * @n: no to convert into binary
- */
+* print_binary - function that pritnts the binary rep of a num.
+* @n: num to convert into binary rep
+*
+*/
 void print_binary(unsigned long int n)
 {
-unsigned long int bit_mask = 1;
-while (bit_mask < n)
-bit_mask = (bit_mask << 1) | 1;
-
-while (bit_mask > 0)
-{
-if (n & bit_mask)
-_putchar('1');
-else
-_putchar('0');
-
-bit_mask >>= 1;
-}
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+_putchar((n & 1) + '0');
 }
 
