f4c39ad5284f4f84003d292aae9c761d68e77739
wanjirumephis <wanjirunduhi@gmail.com>
2023-04-10 07:38:39 -0700

    fixed spaces at the start of line

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,21 +1,24 @@
+#include "main.h"
+
 /**
  * flip_bits - returns the number of bits you would need to flip
  *             to get from one number to another
- * @n: the first number
- * @m: the second number
+ * @n: first number
+ * @m: second number
  *
- * Return: the number of bits needed to flip
+ * Return: the no of bits needed to flip
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int diff = n ^ m;
-    unsigned int count = 0;
+unsigned long int diff = n ^ m;
+unsigned int count = 0;
 
-    while (diff != 0)
-    {
-        count += diff & 1;
-        diff >>= 1;
-    }
- return (count);
+while (diff)
+{
+count++;
+diff &= diff - 1;
+}
+
+return (count);
 }
 
