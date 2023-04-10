b758a5fe353921f4d0ea0cf81252eabd63d9e74d
wanjirumephis <wanjirunduhi@gmail.com>
2023-04-10 08:35:16 -0700

    fixed indentation errors

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,18 +1,17 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
-* print_binary - function that prints the binary representation of a number.
-* @n: number to convert into binary representation
-*
-*/
+ * print_binary - Prints the binary rep of n.
+ * @n: The number to be printed
+ */
+
 void print_binary(unsigned long int n)
 {
 	if (n > 1)
 	{
 		print_binary(n >> 1);
 	}
-_putchar((n & 1) + '0');
-}
 
+	_putchar((n & 1) + '0');
+}
 
