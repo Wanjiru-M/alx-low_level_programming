d4f254f8b8a95c92d4762b5f12fe08d094f2797f
wanjirumephis <wanjirunduhi@gmail.com>
2023-04-11 11:01:26 -0700

    code restructuring

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len = 0;
+	int s, u, len = 0;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,10 +22,10 @@ int append_text_to_file(const char *filename, char *text_content)
 			len++;
 	}
 
-	o = open(filename, O_WRONLY | O_APPEND);
-	w = write(o, text_content, len);
+	s = open(filename, O_WRONLY | O_APPEND);
+	u = write(o, text_content, len);
 
-	if (o == -1 || w == -1)
+	if (s == -1 || u == -1)
 		return (-1);
 
 	close(o);
