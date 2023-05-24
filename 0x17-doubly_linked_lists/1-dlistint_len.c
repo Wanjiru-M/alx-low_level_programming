d728f11ec4ec25b14ce20c49eb23c62c4a93bc78
wanjirumephis <wanjirunduhi@gmail.com>
2023-05-24 01:21:40 -0700

    a function that adds a new node at the beginning of a dlistint_t list

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -2,14 +2,14 @@
 
 /**
  * dlistint_len - Returns the number of elements in a doubly linked list.
- * @head: Pointer to the head of the list.
+ * @h: Pointer to the head of the list.
  *
  * Return: Number of elements in the list.
  */
-size_t dlistint_len(const dlistint_t *head)
+size_t dlistint_len(const dlistint_t *h)
 {
 	size_t count = 0;
-	const dlistint_t *current = head;
+	const dlistint_t *current = h;
 
 	while (current != NULL)
 	{
