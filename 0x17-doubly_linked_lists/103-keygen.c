190cc33d0c708793362075d34222e76f831d38a8
wanjirumephis <wanjirunduhi@gmail.com>
2023-05-26 10:06:43 -0700

    update

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -11,47 +11,50 @@
  */
 int main(int argc, char *argv[])
 {
-	unsigned int index, sum;
-	size_t length, addition;
-	char *charset = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-	char password[7] = "      ";
+    unsigned int index, sum;
+    size_t length, addition;
+    char *charset = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+    char password[7] = "      ";
 
-	if (argc != 2)
-	{
-		printf("Correct usage: ./keygen5 username\n");
-		return 1;
-	}
+    if (argc != 2)
+    {
+        printf("Correct usage: ./keygen5 username\n");
+        return 1;
+    }
 
-	length = strlen(argv[1]);
-	password[0] = charset[(length ^ 59) & 63];
+    length = strlen(argv[1]);
+    password[0] = charset[(length ^ 59) & 63];
 
-	for (index = 0, addition = 0; index < length; index++)
-		addition += argv[1][index];
+    for (index = 0, addition = 0; index < length; index++)
+        addition += argv[1][index];
 
-	password[1] = charset[(addition ^ 79) & 63];
+    password[1] = charset[(addition ^ 79) & 63];
 
-	for (index = 0, sum = 1; index < length; index++)
-		sum *= argv[1][index];
+    for (index = 0, sum = 1; index < length; index++)
+        sum *= argv[1][index];
 
-	password[2] = charset[(sum ^ 85) & 63];
+    password[2] = charset[(sum ^ 85) & 63];
 
-	for (sum = argv[1][0], index = 0; index < length; index++)
-		if ((char)sum <= argv[1][index])
-			sum = argv[1][index];
+    for (sum = argv[1][0], index = 0; index < length; index++)
+    {
+        if ((char)sum <= argv[1][index])
+            sum = argv[1][index];
+    }
 
-	srand(sum ^ 14);
-	password[3] = charset[rand() & 63];
+    srand(sum ^ 14);
+    password[3] = charset[rand() & 63];
 
-	for (sum = 0, index = 0; index < length; index++)
-		sum += argv[1][index] * argv[1][index];
+    for (sum = 0, index = 0; index < length; index++)
+        sum += argv[1][index] * argv[1][index];
 
-	password[4] = charset[(sum ^ 239) & 63];
+    password[4] = charset[(sum ^ 239) & 63];
 
-	for (sum = 0, index = 0; (char)index < argv[1][0]; index++)
-		sum = rand();
+    for (sum = 0, index = 0; (char)index < argv[1][0]; index++)
+        sum = rand();
 
-	password[5] = charset[(sum ^ 229) & 63];
+    password[5] = charset[(sum ^ 229) & 63];
 
-	printf("%s\n", password);
-	return 0;
+    printf("%s\n", password);
+    return 0;
 }
+
