40a14d96ec4f7ce1760623c82c6b268d94a4898e
wanjirumephis <wanjirunduhi@gmail.com>
2023-05-26 10:11:22 -0700

    update

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -1,59 +1,53 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
- * main - generate a key depending on a username for crackme5
- * @argc: number of arguments passed
- * @argv: arguments passed to main
+ * main - Generates and prints passwords for the crackme5 executable.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * Return: 0 on success, 1 on error
+ * Return: Always 0.
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-    unsigned int index, multiplier;
-    size_t length, sum;
-    char *charset = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
-    char password[7] = "      ";
+	char password[7], *codex;
+	int len = strlen(argv[1]), i, tmp;
 
-    if (argc != 2)
-    {
-        printf("Correct usage: ./keygen5 username\n");
-        return 1;
-    }
+	codex = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 
-    length = strlen(argv[1]);
-    password[0] = charset[(length ^ 59) & 63];
+	tmp = (len ^ 59) & 63;
+	password[0] = codex[tmp];
 
-    for (index = 0, sum = 0; index < length; index++)
-        sum += argv[1][index];
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += argv[1][i];
+	password[1] = codex[(tmp ^ 79) & 63];
 
-    password[1] = charset[(sum ^ 79) & 63];
+	tmp = 1;
+	for (i = 0; i < len; i++)
+		tmp *= argv[1][i];
+	password[2] = codex[(tmp ^ 85) & 63];
 
-    for (index = 0, multiplier = 1; index < length; index++)
-        multiplier *= argv[1][index];
+	tmp = 0;
+	for (i = 0; i < len; i++)
+	{
+		if (argv[1][i] > tmp)
+			tmp = argv[1][i];
+	}
+	srand(tmp ^ 14);
+	password[3] = codex[rand() & 63];
 
-    password[2] = charset[(multiplier ^ 85) & 63];
+	tmp = 0;
+	for (i = 0; i < len; i++)
+		tmp += (argv[1][i] * argv[1][i]);
+	password[4] = codex[(tmp ^ 239) & 63];
 
-    for (multiplier = argv[1][0], index = 0; index < length; index++)
-    {
-        if ((char)multiplier <= argv[1][index])
-            multiplier = argv[1][index];
-    }
+	for (i = 0; i < argv[1][0]; i++)
+		tmp = rand();
+	password[5] = codex[(tmp ^ 229) & 63];
 
-    srand(multiplier ^ 14);
-    password[3] = charset[rand() & 63];
-
-    for (sum = 0, index = 0; index < length; index++)
-        sum += argv[1][index] * argv[1][index];
-
-    password[4] = charset[(sum ^ 239) & 63];
-
-    for (sum = 0, index = 0; (char)index < argv[1][0]; index++)
-        sum = rand();
-
-    password[5] = charset[(sum ^ 229) & 63];
-
-    printf("%s\n", password);
-    return 0;
+	password[6] = '\0';
+	printf("%s", password);
+	return (0);
 }
