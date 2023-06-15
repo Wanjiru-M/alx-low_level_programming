a0f31442f5914f75d1de6d3705b6f10923dbc432
wanjirumephis <wanjirunduhi@gmail.com>
2023-06-15 01:37:46 -0700

    fix checker

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -51,4 +51,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new->value = new_value;
 	new->next = ht->array[index];
 	ht->array[index] = new;
-	return (1);}
+	return (1);
+}
